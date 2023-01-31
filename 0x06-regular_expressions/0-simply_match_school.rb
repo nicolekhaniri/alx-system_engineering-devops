3f9511e3248b20004593706d971b850e4ad69d80
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2023-01-31 16:03:44 +0300

    ruby zero script

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+#Get the string input from the command line argument
+input_str = ARGV[0]
+
+#Define the regular expression pattern
+pattern = /School/
+
+#Pass the input string to the regular expression matching method
+if input_str =~ pattern
+  puts "#{input_str}"
+else
+  puts "#{}"
+end
