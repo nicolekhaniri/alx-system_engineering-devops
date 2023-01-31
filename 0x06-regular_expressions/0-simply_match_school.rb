ca064316e18b531f418c5b69ff1a2d784d377094
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2023-01-31 16:30:32 +0300

    two to five occurences of t in between hbtn

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
-#Get the string input from the command line argument
-input_str = ARGV[:1]
-
-#Define the regular expression pattern
-pattern = /School/
-
-#Pass the input string to the regular expression matching method
-if input_str =~ pattern
-  puts "#{input_str}"
-else
-  puts "#{}"
-end
+#Matching School
+puts ARGV[0].scan(/School/).join
