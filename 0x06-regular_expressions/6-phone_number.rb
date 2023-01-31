66b8d759ef5a6f4a011d742754324f37f3143dd5
nicolekhaniri <nicolekhaniri@gmail.com>
2023-01-31 06:17:08 -0800

    executable files tested

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(1234567890{10}).join
+puts ARGV[0].scan(/1234567890{10}/).join
--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scans(/ABCDEFGHIJKLMNOPQRSTUVWXYZ/).join
+puts ARGV[0].scan(/[A-Z]*/).join
